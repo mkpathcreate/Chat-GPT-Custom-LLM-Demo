3ab5b4f63e8baec29478c5111ac975002838a7de
MK-Rhytha <mk@rhytha.com>
2023-10-18 10:17:20 +0900

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # Chat-GPT-Custom-LLM-Demo
 Chat GPT Custom LLM Demo
+
+This is a simple demo of Chat GPT Custom domain LLM and prompt demo. 
+
+
+## Install the below libraries
+
+python3 -m pip install -U pip   
+pip3 install openai                                       
+pip3 install PyPDF2
+pip3 install langchain
+pip3 install llama-index
+pip3 install gradio
+pip3 install transformers    
+pip3 install docx2txt       
+pip3 install protobuf   
+pip3 install sentencepiece             
+pip3 install torch             
+pip3 install gpt_index
+pip3 install PyCryptodome
+pip3 install langchain langchain-experimental       
+pip3 install torch transformers python-pptx Pillow
+
+## Add files for Indexing
+
+Copy txt, pdf, excel, docx files into docs folder, if you use a existing folder, make sure you dont have CSV files or other files, removed, since all files are not indexable by this code.
+
+## RUN python3 model.py 
+
+## Access via Web Browser
+
+If you dont get any error (you are lucky !!!), a localhost link is created, open this link and ask questions to Chat GPT on the files you have put in docs folder.
+
+Running on local URL:  http://127.0.0.1:7860
+
+### Reference URL
+
+Tutorial -> The link has old functions which has changed, check the second link for new functions list, MK has fixed the code
+as on Oct 17, 2023, but the code seems to change fast, some within 1 month, so might not work in future.
+
+https://medium.com/rahasak/creating-custom-chatgpt-with-your-own-dataset-using-openai-gpt-3-5-model-llamaindex-and-langchain-5d5837bf9d56
+
+Reference documentation to fix the Code in case of errors.
+https://gpt-index.readthedocs.io/en/stable/end_to_end_tutorials/usage_pattern.html
---
+++ model.py
@@ -0,0 +1,85 @@
+from llama_index import SimpleDirectoryReader, VectorStoreIndex, LLMPredictor, ServiceContext, PromptHelper
+from llama_index import StorageContext, load_index_from_storage
+from llama_index import ServiceContext
+from langchain.chat_models import ChatOpenAI
+from llama_index.llms import OpenAI
+from langchain.utilities import SQLDatabase
+from langchain.llms import OpenAI
+from langchain_experimental.sql import SQLDatabaseChain
+import gradio as gr
+import sys
+import os
+
+
+#os.environ["OPENAI_API_KEY"] = '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'
+os.environ["OPENAI_API_KEY"] = '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'
+TOKENIZERS_PARALLELISM = False
+
+# Enable this if you enable local Chat GPT to cut costs, but will not work with gradio web interface
+# service_context = ServiceContext.from_defaults(llm="local")
+
+
+def init_index(directory_path):
+    # model params
+    # max_input_size: maximum size of input text for the model.
+    # num_outputs: number of output tokens to generate.
+    # max_chunk_overlap: maximum overlap allowed between text chunks.
+    # chunk_size_limit: limit on the size of each text chunk.
+    max_input_size = 4096
+    num_outputs = 512
+    max_chunk_overlap = 0
+    chunk_size_limit = 600    
+
+    # llm predictor with langchain ChatOpenAI
+    # ChatOpenAI model is a part of the LangChain library and is used to interact with the GPT-3.5-turbo model provided by OpenAI
+    prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
+    
+    #Costs Money, sends the query to ChatGPT API
+    llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0.7, model_name="gpt-4", max_tokens=num_outputs))
+    
+    #Local ChatGPT, but this cannot use web interface as of now so commented it out
+    #llm_predictor = OpenAI(model="gpt-4", temperature=0, max_tokens=num_outputs)
+
+    # read documents from docs folder
+    documents = SimpleDirectoryReader(directory_path).load_data()
+
+    # init index with documents data
+    # This index is created using the LlamaIndex library. 
+    # It processes the document content and constructs the index to facilitate efficient querying
+    service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+    index = VectorStoreIndex.from_documents(documents, service_context=service_context)
+
+    # save the created index
+    #index.storage_context.persist
+    index.storage_context.persist(persist_dir="/Users/mk/Projects/Custom-Domain-Training-AI/chatbot/db")
+
+
+    return index
+
+def chatbot(input_text):
+    # load index
+    storage_context = StorageContext.from_defaults(persist_dir="/Users/mk/Projects/Custom-Domain-Training-AI/chatbot/db")
+    index = load_index_from_storage(storage_context)
+    # SummaryIndexRetriever
+    retriever = index.as_retriever(retriever_mode='default')
+    # get response for the question
+    query_engine = index.as_query_engine()
+    #response = query_engine.query(retriever, response_mode="compact")
+    #query_engine = RetrieverQueryEngine.from_args(retriever, response_mode='compact')
+    response = query_engine.query(input_text)
+    #response = index.query(input_text, response_mode="compact")
+
+
+    return response.response
+
+# create index
+#init_index("/Users/mk/Google Drive/Proposals")
+init_index("docs")
+# create ui interface to interact with gpt-3 model
+iface = gr.Interface(fn=chatbot,
+                     inputs=gr.components.Textbox(lines=7, placeholder="Enter your question here"),
+                     outputs="text",
+                     title="Rhytha AI ChatBot: Your Knowledge Companion Powered-by ChatGPT",
+                     description="Ask any questions",
+                     allow_screenshot=True)
+iface.launch(share=True)
